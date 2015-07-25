eeb1b40d73cd01679b346b57f9ed4a1939f466c3
sut <gh1010801310@gamil.com>
2015-07-25 13:13:51 +0900

    da i jho u bu ka ?

---
+++ mikura/computer/20/aaa.lua
@@ -0,0 +1,15 @@
+﻿	--[[if x % 2 == 1 then
+			t.turnLeft()
+			for j = 0, x - 2 do
+				m.move("front")
+			end
+			t.turnLeft()
+			for k = 0, y - 2 do
+				m.move("front")
+			end
+		else
+			t.turnRight()
+			for j = 0, x - 2 do
+				m.move("front")
+			end
+		end	]]--
